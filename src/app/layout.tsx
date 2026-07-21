0d783d77606d008bdf483060f5c95b53d78444a8
mohammad jiyauddin <kbarkat544@gmail.com>
2026-07-21 08:19:16 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
-
+// ...
 const heading = Space_Grotesk({
   variable: "--font-heading",
   subsets: ["latin"],
